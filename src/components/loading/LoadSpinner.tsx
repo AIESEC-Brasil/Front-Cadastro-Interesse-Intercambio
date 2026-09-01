a311c143625682ddd47f6c994751682364c7f4c8
Caio-Marinho <kaigabriel12@gmail.com>
2026-09-01 17:03:58 -0300

    ajustando espaço

--- src/components/loading/LoadSpinner.tsx
+++ src/components/loading/LoadSpinner.tsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import styles from './style.module.css';
 
-
 import type { LoadSpinnerProps } from '../../types/componentes';
 
 /** Bloqueia a interação durante operações assíncronas de envio. */
